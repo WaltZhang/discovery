e9cadb359bfe6aad1c117a3417ade3659a33475d
WaltZhang <walt.zhangwenguang@gmail.com>
2017-10-24 08:31:53 +0800

    Job's Rest Methods

--- datainventory/views.py
+++ datainventory/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
+
 from datainventory.models import InventoryModel
 
 
-def inventory_list():
+def inventory_list(request):
     object_list = InventoryModel.objects.all()
+    context = {
+        'object_list': object_list
+    }
+    return render(request, 'datainventory/list.html', context=context)
