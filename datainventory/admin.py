722f4b734d9bed2e043444ce298b862923cfc86e
WaltZhang <walt.zhangwenguang@gmail.com>
2017-10-30 00:12:48 +0800

    Job's Rest Methods

--- datainventory/admin.py
+++ datainventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from datainventory.models import InventoryModel
+
+
+class InventoryModelAdmin(ModelAdmin):
+    list_display = ['name', 'schema']
+    class Meta:
+        model = InventoryModel
+
+
+admin.site.register(InventoryModel, InventoryModelAdmin)
+
