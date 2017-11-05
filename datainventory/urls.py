4806134a4d49915ee8e6737220f5e3171f71eedb
WaltZhang <walt.zhangwenguang@gmail.com>
2017-11-05 23:48:11 +0800

    Job's Rest Methods

--- datainventory/urls.py
+++ datainventory/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from datainventory import views
 from datainventory import viewsets
 
+app_name = 'inventory'
 urlpatterns = [
-    url(r'^$', views.inventory_list),
-    url(r'^(?P<id>\d+)/$', views.inventory_detail),
+    url(r'^$', views.inventory_list, name='data_list'),
+    url(r'^(?P<id>\d+)/$', views.inventory_detail, name='data_detail'),
     url(r'^api/$', viewsets.inventory_list),
     url(r'^api/(?P<id>\d+)/$', viewsets.inventory_detail)
 ]
